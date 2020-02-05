1e4f0d8cfa94a3bea8c90df24e233cb6102f4ab2
Kamilen <kamil.gluza@gmail.com>
2020-02-05 20:03:54 +0100

    Added some functionalities. SImple SQL queries

--- registerwindow.cpp
+++ registerwindow.cpp
@@ -1,8 +1,9 @@
 #include "registerwindow.h"
 #include "ui_registerwindow.h"
-#include <QtSql/QSqlDatabase>
-#include <QtSql/QSql>
+#include <QtSql>
+#include <QSqlQuery>
 #include <QDebug>
+#include <QRegExpValidator>
 
 RegisterWindow::RegisterWindow(QDialog *parent) :
     QDialog(parent),
@@ -11,6 +12,16 @@ RegisterWindow::RegisterWindow(QDialog *parent) :
     ui->setupUi(this);
     connect(ui->registerButton, SIGNAL(clicked()), this, SLOT(ProcessRegister()));
     connect(ui->cancelButton, SIGNAL(clicked()), this, SLOT(CloseWindow()));
+
+    /*QRegExp rx("");
+    rx.setPattern("");
+
+    ui->firstNameEdit->setValidator( new QRegExpValidator(rx, this) );
+    ui->secondNameEdit->setValidator( new QRegExpValidator(rx, this) );
+    ui->loginEdit->setValidator( new QRegExpValidator(rx, this) );
+    ui->pwdEdit->setValidator( new QRegExpValidator(rx, this) );
+    ui->pwdRepeatEdit->setValidator( new QRegExpValidator(rx, this) );
+    ui->emailEdit->setValidator( new QRegExpValidator(rx, this) );*/
 }
 
 RegisterWindow::~RegisterWindow()
@@ -26,16 +37,84 @@ void RegisterWindow::ExecRegisterWindow()
 
 void RegisterWindow::ProcessRegister()
 {
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("localhost");
-    db.setDatabaseName("projekt");
-    db.setUserName("root");
-    db.setPassword("");
-    if(db.open())
+    bool ifNothingIsEmpty = true;
+
+    ui->firstNameEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+    ui->secondNameEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+    ui->loginEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+    ui->pwdEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+    ui->pwdRepeatEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+    ui->emailEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(110,110,110);padding-top:6px;padding-bottom:6px;}");
+
+    if(ui->firstNameEdit->text().isEmpty())
     {
-        qDebug()<<"You have connected with database!";
+        ui->firstNameEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
     }
-    close();
+    if( ui->secondNameEdit->text().isEmpty())
+    {
+        ui->secondNameEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
+    }
+    if( ui->loginEdit->text().isEmpty())
+    {
+        ui->loginEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
+    }
+    if( ui->pwdEdit->text().isEmpty())
+    {
+        ui->pwdEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
+    }
+    if( ui->pwdRepeatEdit->text().isEmpty())
+    {
+        ui->pwdRepeatEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
+    }
+    if( ui->emailEdit->text().isEmpty())
+    {
+        ui->emailEdit->setStyleSheet("QLineEdit{font-size:14px;border-radius:6px;background-color:rgb(180,0,0);padding-top:6px;padding-bottom:6px;}");
+        ifNothingIsEmpty = false;
+    }
+
+    if( ui->pwdEdit->text()!= ui->pwdRepeatEdit->text() && ifNothingIsEmpty )
+    {
+        ifNothingIsEmpty = false;
+    }
+
+    if( ifNothingIsEmpty )
+    {
+        QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+        db.setDatabaseName("C:/Users/Kamil/Desktop/projekt.db");
+        if(db.open())
+        {
+            QSqlQuery query;
+            query.exec("SELECT COUNT(*) FROM Uzytkownicy");
+            if( query.isSelect() )
+            {
+                query.first();
+                int latestID = query.value(0).toInt();
+
+                //query.exec("INSERT INTO Uzytkownicy VALUES (1, 'Kamil', 'Ciesielski', 'kamil5640', 'haslo123', 'kamil.ciesielski@xd.pl', 0 );");
+
+
+
+                query.exec("INSERT INTO Uzytkownicy VALUES("+QString::number(latestID)+", '"+ui->firstNameEdit->text()+"','"+
+                     ui->secondNameEdit->text()+"','"+ui->loginEdit->text()+"','"+ui->pwdEdit->text()+"','"+ui->emailEdit->text()+
+                           "',"+QString::number(0));
+
+                QSqlError err = query.lastError();
+                qDebug()<< err.text();
+
+            }
+            else
+            {
+                //ERROR
+            }
+            db.close();
+        }
+        close();
+    }
 }
 
 void RegisterWindow::CloseWindow()
